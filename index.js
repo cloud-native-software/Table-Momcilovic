45ff0bd95cb2d10a5ff40ba9f314dd3e7037c78e
DESKTOP-BPV6P2R\marko <markomomcilovic62@gmail.com>
2023-12-12 02:11:33 +0100

    gitinit

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 console.log("Array")
-arr = [1,2,3,4,5,6,7,8,9]
+let arr = [1,2,3,4,5,6,7,8,9]
 console.log(arr)
 
+console.log("MINIMUM")
 min = Math.min(...arr)
 console.log(min)
+
+console.log("MAXIMUM")
+max = Math.max(...arr)
+console.log(max)
+console.log ("Average")
+let sum = 0
+for  ( let number in arr) {
+    sum = sum + number
+}
+average = sum / arr.lenght
+console.log(average)
