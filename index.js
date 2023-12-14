e448aa2c524acf67809f87ca4178207e2198e819
DESKTOP-BPV6P2R\marko <markomomcilovic62@gmail.com>
2023-12-14 02:31:24 +0100

    adding numbers to array and basic info about array

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("Array")
-let arr = [1, 3, 23, -2, 23232, 2132, 65, 7, 8]
+let arr = [2,4]
 console.log(arr)
 let min = arr[0]
 let max = arr[0]
@@ -14,3 +14,16 @@ for (let i = 0; i < arr.length; i++) {
 }
 console.log('min je ' +min)
 console.log('max je ' +max)
+function avg (){
+    let sum = 0
+    for (let i = 0; i< arr.length; i++){
+        sum =+ arr[i] 
+    }
+    let average = sum / arr.length
+    console.log('average is '+ average)
+}
+function sort(){
+    let arr_reverse = arr.sort(function(a, b){return b - a});
+    console.log(arr_reverse)
+}
+sort()
--- vezbanje.js
+++ vezbanje.js
@@ -1,5 +1,58 @@
-let arr = [1, 3, 23, -2, 23232, 2132, 65, 7, 8]
+const readline = require('readline');
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i])
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+list = []
+p = []
+n = []
+
+enter_number()
+function enter_number() {
+    rl.question('Type number from 0 to 99 (0 for results)', (number) => {
+        number = parseInt(number)
+
+        if (number > 0 && number < 99) {
+            list.push(number)
+            
+            enter_number()
+
+        } else if (number < 0 || number > 99) {
+            console.log("Enter number beetween 0 and 99 (except 0)")
+            enter_number()
+        } else if (number === 0) {
+            let min = list[0]
+            let max = list[0]
+            let sum = 0
+            for (let i = 0; i < list.length; i++) {
+                if (min > list[i]){
+                    min = list[i]
+                }
+                if (max < list [i]){
+                    max = list[i]
+                }
+                sum =+ list[i]
+                
+            }
+            let avg = sum / list.length
+            console.log(list)
+            console.log ('Minimum of this array is :'+min)
+            console.log('Maximum of this array is'+max)
+            console.log ('Average is :'+ avg)
+            console.log ('Parni brojevi su:'+ p)
+            console.log ('Neparni brojevi su :'+ n)
+            rl.close()
+
+        }
+        if (number % 2 === 0){
+            p.push(number)
+        }else {
+            n.push(number)
+        }
+
+    }
+    )
 }
+
+
